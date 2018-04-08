ece7ea6b434d095c635b9c4afd22a41c9c17bdd0
hrehman200 <hrehman200@gmail.com>
2018-04-08 23:16:59 +0500

    Flow changed so that browser is used to interact with the app instead of php cli

--- index.php
+++ index.php
@@ -16,28 +16,32 @@ function getClient() {
     $client->setAccessType('offline');
     $client->setApprovalPrompt('force');
 
-    // Load previously authorized credentials from a file.
-    $credentialsPath = CREDENTIALS_PATH;
-    if (file_exists($credentialsPath)) {
-        $accessToken = json_decode(file_get_contents($credentialsPath), true);
+    if (isset($_GET['code'])) {
+        if (strval($_SESSION['state']) !== strval($_GET['state'])) {
+            die('The session state did not match.');
+        }
+
+        $accessToken = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+
+        // Store the credentials to disk.
+        if (!file_exists(dirname(CREDENTIALS_PATH))) {
+            mkdir(dirname(CREDENTIALS_PATH), 0700, true);
+        }
+        file_put_contents(CREDENTIALS_PATH, json_encode($accessToken));
+        printf("Credentials saved to %s\n", CREDENTIALS_PATH);
+
+        $_SESSION['token'] = $client->getAccessToken();
+        header('Location: ' . REDIRECT_URL);
+    }
+
+    if (file_exists(CREDENTIALS_PATH)) {
+        $accessToken = json_decode(file_get_contents(CREDENTIALS_PATH), true);
+        $client->setAccessToken($accessToken);
     } else {
         // Request authorization from the user.
         $authUrl = $client->createAuthUrl();
-        printf("Open the following link in your browser:\n%s\n", $authUrl);
-        print 'Enter verification code: ';
-        $authCode = urldecode(trim(fgets(STDIN)));
-
-        // Exchange authorization code for an access token.
-        $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-
-        // Store the credentials to disk.
-        if (!file_exists(dirname($credentialsPath))) {
-            mkdir(dirname($credentialsPath), 0700, true);
-        }
-        file_put_contents($credentialsPath, json_encode($accessToken));
-        printf("Credentials saved to %s\n", $credentialsPath);
+        header('Location:' . $authUrl);
     }
-    $client->setAccessToken($accessToken);
 
     // Refresh the token if it's expired.
     if ($client->isAccessTokenExpired()) {
@@ -48,23 +52,13 @@ function getClient() {
         $newAccessToken = $client->getAccessToken();
         $accessToken = array_merge($jsonArray, $newAccessToken);
 
-        file_put_contents($credentialsPath, json_encode($accessToken));
+        file_put_contents(CREDENTIALS_PATH, json_encode($accessToken));
     }
     return $client;
 }
 
 $client = getClient();
 $youtube = new Google_Service_YouTube($client);
-
-if (isset($_GET['code'])) {
-    if (strval($_SESSION['state']) !== strval($_GET['state'])) {
-        die('The session state did not match.');
-    }
-
-    $client->fetchAccessTokenWithAuthCode($_GET['code']);
-    $_SESSION['token'] = $client->getAccessToken();
-    header('Location: ' . REDIRECT_URL);
-}
 
 if (isset($_SESSION['token'])) {
     $client->setAccessToken($_SESSION['token']);
@@ -98,9 +92,8 @@ if ($playlistItems) {
     $saved_video_id = file_get_contents(LATEST_VIDEO_PATH);
 
     if($latest_video_id != $saved_video_id) {
-        echo $item['snippet']['title'] . " " . $latest_video_id . "\n";
+        echo $item['snippet']['title'] . " " . $latest_video_id . "<br>";
         file_put_contents(LATEST_VIDEO_PATH, $latest_video_id);
-
 
         $twitter_token = json_decode(file_get_contents(TWITTER_OAUTH_PATH), true);
         $twitter = new TwitterOAuth(
@@ -118,9 +111,9 @@ if ($playlistItems) {
 
         echo ('Created new twitter status with #' . $status->id . PHP_EOL);
 
+    } else {
+        echo 'No new video found in playlist to be tweeted';
     }
 }
-
-
 
 ?>
