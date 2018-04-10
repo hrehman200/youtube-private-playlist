2a9e02af65180c2354f9eac625c60352264563c4
hrehman200 <hrehman200@gmail.com>
2018-04-10 11:11:35 +0500

    Minor adjustment

--- twitter_callback.php
+++ twitter_callback.php
@@ -7,7 +7,7 @@
  */
 
 require_once __DIR__ . '/vendor/autoload.php';
-require_once 'config.php';
+require_once __DIR__ . '/config.php';
 session_start();
 
 use Abraham\TwitterOAuth\TwitterOAuth;
--- twitter_login.php
+++ twitter_login.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once 'vendor/autoload.php';
-require_once 'config.php';
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/config.php';
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
